db00762bc6e28680ea441fb50350e89d871eb2bd
AddieEli <acadavid71@gmail.com>
2017-08-12 12:10:58 -0500

    new practice

--- duplicates.rb
+++ duplicates.rb
@@ -1,3 +1,18 @@
 #a range of 1..100 how do you find duplicates?
 
-range = (1..5)
+range = [1,4,4,5,6,7,7,8]
+
+correct_nums = []
+double = []
+
+range.each do |num|
+ unless correct_nums.include? num
+    correct_nums << num
+  else
+    double << num
+  end
+end
+
+
+p correct_nums
+p double
